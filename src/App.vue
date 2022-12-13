b6b93b3ea3222418bb2b9d371ff1bcdaa252f465
Jackietruong2805 <truonghao2805@gmail.com>
2022-12-13 17:24:15 +0700

    make it better

---
+++ src/App.vue
@@ -0,0 +1,111 @@
+<script setup>
+// import LoveBg from "../public/img/LoveBg.avif"
+let date = new Date();
+let Hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+setInterval(()=>{
+  
+}, 1000);
+
+</script>
+
+<template>
+    <div class="wrapper">
+        <div class="container set-time">
+            <section>
+                <h1 class="title">Nhập vào Ngày bắt đầu yêu</h1>
+                <div class="set-date">
+                    <input class="timer-start" id="start" type="date" min="2021-12-31">
+                </div>
+            </section>
+            <section>
+                <button class="start-button">Bắt đầu</button>
+            </section>
+        </div>
+        <div class="timer container">
+            <div class="timer-item"></div>
+            <div class="timer-item"></div>
+            <div class="timer-item"></div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.wrapper {
+  max-width: 1600px;
+  background-image: url("../img/love3Bg.avif");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+}
+
+.container{
+  max-width: 1300px;
+  margin: 0 auto;
+  padding: 15px 0;
+}
+
+.set-time{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100%;
+  gap: 30px 0;
+}
+
+.timer{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.title{
+  font-weight: bold;
+  font-size: 40px;
+  color: white;
+  font-family: 'Rubik Gemstones', cursive;
+  margin-bottom: 45px;
+}
+
+.set-date{
+  max-width: 300px;
+  width: 250px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: bisque;
+  border: 2px solid gray;
+  border-radius: 20px;
+  padding: 0 15px;
+  gap: 0 20px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+}
+.timer-start, .timer-start:focus{
+  border: none;
+  padding: 5px;
+  background-color: transparent;
+  font-size: 30px;
+  color: blueviolet;
+  outline: none;
+}
+input[type="date"]::-webkit-inner-spin-button,
+input[type="date"]::-webkit-calendar-picker-indicator {
+  font-size: 30px;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+}
+
+.start-button{
+  padding: 15px 30px;
+  border-radius: 20px;
+  border: none;
+  background: green;
+  color: white;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import "../src/assets/css/reset.css"
+createApp(App).mount('#app')
